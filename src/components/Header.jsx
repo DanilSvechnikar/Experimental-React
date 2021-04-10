8727363679060dbcb6db4de15830025213755944
DanilSvechnikar <https://github.com/DanilSvechnikar>
2021-04-10 21:20:37 +0300

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+	return (
+		<header className='header'>
+			<img src='https://www.bravado.de/assets/asset_300x300/P5054190307789_4.jpg' />
+		</header>
+	);
+}
+
+
+export default Header;
