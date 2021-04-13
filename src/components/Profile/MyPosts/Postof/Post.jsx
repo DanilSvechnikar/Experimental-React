4485a5be9eee415cb49c059790f700169a3ad961
DanilSvechnikar <https://github.com/DanilSvechnikar>
2021-04-13 20:54:00 +0300

    studied props

--- src/components/Profile/MyPosts/Postof/Post.jsx
+++ src/components/Profile/MyPosts/Postof/Post.jsx
@@ -1,15 +1,19 @@
 import s from './Post.module.css'
 
 
-const Post = () => {
+const Post = (props) => {
 	return (
 		<div className={s.item}>
 			<img src='https://yt3.ggpht.com/a/AATXAJwmjVEDKXOZCkT8JhOHaUIohfBGziaQolfxkw=s900-c-k-c0xffffffff-no-rj-mo'></img>
-			Post 1
+			{ props.message }
+
 			<div>
 				<span>like</span>
 			</div>
+			
 		</div>
+
+		
 	);
 }
 
