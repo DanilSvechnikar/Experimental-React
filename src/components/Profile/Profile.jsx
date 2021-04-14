107526de9c5e67bf40782ebd448ecf81380622b9
DanilSvechnikar <https://github.com/DanilSvechnikar>
2021-04-14 19:55:14 +0300

    added Dialogs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,9 @@
 import MyPosts from './MyPosts/MyPosts';
-import s from './Profile.module.css';
 
 
 const Profile = () => {
 	return (
-		<div className={s.content}>
+		<div>
 			<div>
 				<img src='http://media.digitalphotogallery.com/pruemsozblzo/images/3e466c1e-4db0-11e7-966e-8ef06f2f9be6/motionless_in_white39_website_image_dtdr_standard.jpg?20170610100306' height='200px' width='300px' />
 			</div>
